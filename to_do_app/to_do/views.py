7ea9e06d95df51437f70925d8ff5d2ffb6f77695
Logan Cox <mail@logan-cox.com>
2022-07-05 21:00:54 +0100

    Adding class view for task list

--- to_do_app/to_do/views.py
+++ to_do_app/to_do/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import Task
 
 
-def task_list(request):
-    return HttpResponse('To do app!')
+class TaskList(ListView):
+    model = Task
+
 
