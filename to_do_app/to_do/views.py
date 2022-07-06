2490d6bfb56264f9c09a9d84c4230873af7c29b8
Logan Cox <mail@logan-cox.com>
2022-07-06 19:47:19 +0100

    Create task class view

--- to_do_app/to_do/views.py
+++ to_do_app/to_do/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Task
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+    template_name = 'to_do/create_task.html'
 
 
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
     template_name = 'to_do/all_tasks.html'
+
 
 class TaskDetail(DetailView):
     model = Task
