3f09eada85a55d37776114b76fc74499cc26329c
Logan Cox <mail@logan-cox.com>
2022-07-06 20:01:45 +0100

    Update view

--- to_do_app/to_do/views.py
+++ to_do_app/to_do/views.py
@@ -1,11 +1,18 @@
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Task
 
 
 class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+    template_name = 'to_do/create_task.html'
+
+
+class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
