d37e7e8a1e24882f7dadd0c89f9a365ae7cdad57
Logan Cox <mail@logan-cox.com>
2022-07-09 16:00:47 +0100

    only show users their own tasks

--- to_do_app/to_do/views.py
+++ to_do_app/to_do/views.py
@@ -10,14 +10,18 @@ from .models import Task
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
     template_name = 'to_do/create_task.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
 
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
     template_name = 'to_do/create_task.html'
 
@@ -33,6 +37,12 @@ class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
     template_name = 'to_do/all_tasks.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count
+        return context
 
 
 class TaskDetail(LoginRequiredMixin, DetailView):
